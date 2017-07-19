e8a597ab4b1c7650a9178e36eeeaaed5dc6f905d
vijaimanikandan <vijaimanikandan@gmail.com>
2017-07-20 01:00:56 +0530

    added imdb initial setup

--- app.py
+++ app.py
@@ -1,8 +1,73 @@
 import sqlite3
 from flask import Flask, render_template, json, request, g
+from bs4 import BeautifulSoup
+import requests
+import json
 import base64
 app = Flask(__name__)
 DATABASE = './test.db'
+
+
+
+def getJSON(html):
+			
+	data = {}
+	data['poster'] = html.find(attrs={'class':'poster'}).find('img')['src']
+	data['title'] =  html.find(itemprop='name').text.strip()
+	data['rating'] = html.find(itemprop='ratingValue').text
+	data['bestRating'] = html.find(itemprop='bestRating').text
+	data['votes'] = html.find(itemprop='ratingCount').text
+	data['rated'] = html.find(itemprop='contentRating')['content']
+	tags = html.findAll("span",{"itemprop":"genre"})
+	genres = []
+	for genre in tags:
+		genres.append(genre.text.strip())
+	data['genre'] = genres	
+		
+	data['description'] = html.find(itemprop="description").text.strip()
+
+	tags = html.findAll(itemprop="actors")
+	actors = []
+	for actor in tags:
+		actors.append(actor.text.strip().replace(',',''))
+	data['cast'] = actors	
+		
+
+	tags = html.findAll(itemprop="creator")
+	creators = []
+	for creator in tags:
+		creators.append(creator.text.strip().replace(',',''))
+	data['writers'] = creators	
+		
+	directors = []
+	tags = html.findAll(itemprop="director")
+	for director in tags:
+		directors.append(director.text.strip().replace(',',''))
+	data['directors'] = directors	
+		
+	json_data = json.dumps(data)
+	return json_data
+	
+	
+def getHTML(url):
+	response = requests.get(url)
+	return BeautifulSoup(response.content,'html.parser')	
+	
+	
+def getURL(input):
+	try:
+		if input[0] == 't' and input[1] == 't':
+			html = getHTML('http://www.imdb.com/title/'+input+'/')
+			
+		else:
+			html = getHTML('https://www.google.co.in/search?q='+input)
+			for cite in html.findAll('cite'):
+				if 'imdb.com/title/tt' in cite.text:
+					html = getHTML('http://'+cite.text)
+					break
+		return getJSON(html)	
+	except Exception as e:
+		return 'Invalid input or Network Error!'
 
 
 def get_db():
@@ -62,6 +127,11 @@ def showsignin():
     return render_template("signin.html")
 
 
+@app.route('/getImdb')
+def getimdb():
+	return flask.jsonify(getURL('pulp fiction'))
+
+
 @app.route('/signUp', methods=['POST'])
 def signUp():
     # create_testdb()
@@ -97,6 +167,7 @@ def signIn():
     else:
         return json.dumps({'error': 'Password does not match!'})
 
+		
 
 if __name__ == '__main__':
     app.run(debug=True)
